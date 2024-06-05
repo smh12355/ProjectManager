3d2e51ce470fdb2dd076485cecbfa4689c372d3a
smh <krik6162@gmail.com>
2024-06-05 13:56:52 +0500

    add startup.cs

--- src/ProjectManager/Program.cs
+++ src/ProjectManager/Program.cs
@@ -11,45 +11,12 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        //контроллеры
-        builder.Services.AddControllers();
-        //swagger
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        //db и сервисы
-        builder.Services.AddInfrastructureServices(builder.Configuration);
-        builder.Services.AddApplicationServices();
+        var startup = new Startup(builder.Configuration, builder.Environment);
+        startup.ConfigureServices(builder.Services);
 
-        builder.Services.AddControllers(options =>
-        {
-            options.Filters.Add<ExceptionFilter>();
-        });
         var app = builder.Build();
 
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            try
-            {
-                var context = services.GetRequiredService<ProjectManagerDbContext>(); // «амените на ваш DbContext
-                context.Database.Migrate();
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred while migrating or initializing the database.");
-            }
-        }
-        // смотрим launchSettings.json
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.MapControllers();
+        startup.Configure(app);
 
         app.Run();
     }
---
+++ src/ProjectManager/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using ProjectManager.Filters;
+using ProjectManager.Infrastructure;
+using ProjectManager.Application;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManager;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _environment;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        _configuration = configuration;
+        _environment = environment;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Контроллеры
+        services.AddControllers();
+
+        // Swagger
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        // DB и сервисы
+        services.AddInfrastructureServices(_configuration);
+        services.AddApplicationServices();
+
+        services.AddControllers(options =>
+        {
+            options.Filters.Add<ExceptionFilter>();
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<ProjectManagerDbContext>(); // Замените на ваш DbContext
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Startup>>();
+                logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+            }
+        }
+
+        // Смотрим launchSettings.json
+        if (_environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
